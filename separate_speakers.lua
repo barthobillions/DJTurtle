e3b245129e083923a1831cc0c66cc5440225cf52
Nhat <58672082+gnatttt@users.noreply.github.com>
2022-12-19 04:16:27 -0500

    wired modem change and comments

--- separate_speakers.lua
+++ separate_speakers.lua
@@ -1,6 +1,10 @@
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
+local modem = peripheral.wrap("back")
+modem.open(5)
 
+--Takes in file path and reads its data
+--The decoder converts its to data the speaker can play
 function load_and_play_audiostream(file)
 	local decoder = dfpwm.make_decoder()
 	for chunk in io.lines(file, 16 * 1024) do
@@ -12,10 +16,11 @@ function load_and_play_audiostream(file)
 	end
 end
 
+--Waits for main turtle to transmit data through the modem
+--Once something is recieved, it will load and play the songs
 function main()
-	rednet.open("back")
 	while true do
-		local _,file = rednet.receive()
+		local _, _, _, _, file = os.pullEvent("modem_message")
 		load_and_play_audiostream(file)
 	end
 
